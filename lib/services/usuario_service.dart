be522a424590613c5f115345e74d8e3499ad6f12
carlosdarioio <cdfn3@hotmail.com>
2021-03-14 21:33:49 -0600

    suamdo numeros
    testetiando el provider sumando dos numeros

--- lib/services/usuario_service.dart
+++ lib/services/usuario_service.dart
@@ -1,32 +1,11 @@
 import 'package:cf_flutter/models/numeros.dart';
 import 'package:flutter/material.dart';
 
+class UsuarioService with ChangeNotifier {
+  int numero1 = 0;
+  int numero2 = 0;
 
-
-class UsuarioService with ChangeNotifier { 
-
-  Numeros _numero;
-
-  Numeros get numeros => this._numero;
-  bool get existeUsuario => (this._numero != null) ? true : false;
-
-  set usuario( Numeros nums ) {
-    this._numero = nums;
-    notifyListeners();
+  int sumar() {
+    return numero1 + numero2;
   }
-
-  void cambiarNum1( int num ) {
-    this._numero.num1 = num;
-    notifyListeners();
-  }
-  void cambiarNum2( int num ) {
-    this._numero.num2 = num;
-    notifyListeners();
-  }
-
-  void removerNumeros(){
-    this._numero = null;
-    notifyListeners();
-  }  
-
 }
